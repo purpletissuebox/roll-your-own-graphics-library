c6b80ab36eba90deb7a11279e1b164b34a05d3bf
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-08-17 14:50:05 -0400

    hook all the routines into main

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include "gfx/os.h"
+#include "gfx/gfxbuffer.h"
+#include "gfx/draw.h"
+
+#include "stdio.h"
+#include "stdlib.h"
 
 int main(void)
 {
 	void *stdio = GetStdIO();
 	int x, y;
 	QueryConsoleSize(stdio, &x, &y);
+	CreateGfxBuffers(x, y, 1.6);
+	DrawTriangle(35, 0, 50, 4, 20, 14, COLOR(0, 5, 0));
+	RenderCanvas();
+	puts((char*)gfx_string);
 }
 
