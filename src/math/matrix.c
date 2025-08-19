cf7076729ec1e9fc4036aaa3746746bb21c5427b
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-08-19 17:31:02 -0400

    add routines to create transforms from scalars. apply all matrix ops to 4x4.

--- src/math/matrix.c
+++ src/math/matrix.c
@@ -2,31 +2,131 @@
 
 #include "math.h"
 
-void FillRotationMatrix(double a, double b, double c, double(*matrix)[3])
+void FillRotationMatrix(double a, double b, double c, double(*matrix)[4])
 {
-	matrix[0][0] = cos(b) * cos(c);
-	matrix[0][1] = sin(a) * sin(b) * cos(c) - cos(a) * sin(c);
-	matrix[0][2] = cos(a) * sin(b) * cos(c) + sin(a) * sin(c);
-	matrix[1][0] = cos(b) * sin(c);
-	matrix[1][1] = sin(a) * sin(b) * sin(c) + cos(a) * cos(c);
-	matrix[1][2] = cos(a) * sin(b) * sin(c) - sin(a) * cos(c);
-	matrix[2][0] = sin(b) * -1.0;
-	matrix[2][1] = sin(a) * cos(b);
-	matrix[2][3] = cos(a) * cos(b);
+	double *p = matrix;
+	*p++ = cos(b) * cos(c);
+	*p++ = sin(a) * sin(b) * cos(c) - cos(a) * sin(c);
+	*p++ = cos(a) * sin(b) * cos(c) + sin(a) * sin(c);
+	*p++ = 0.0;
+	*p++ = cos(b) * sin(c);
+	*p++ = sin(a) * sin(b) * sin(c) + cos(a) * cos(c);
+	*p++ = cos(a) * sin(b) * sin(c) - sin(a) * cos(c);
+	*p++ = 0.0;
+	*p++ = sin(b) * -1.0;
+	*p++ = sin(a) * cos(b);
+	*p++ = cos(a) * cos(b);
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 1.0;
 }
 
-void MatrixMultiply3x3(double(*matrix)[3], double *vector, double *result)
+void FillScaleMatrix(double x, double y, double z, double(*matrix)[4])
+{
+	double *p = matrix;
+	*p++ = x;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = y;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = z;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 1.0;
+}
+
+void FillTranslationMatrix(double x, double y, double z, double(*matrix)[4])
+{
+	double *p = matrix;
+	*p++ = 1.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = x;
+	*p++ = 0.0;
+	*p++ = 1.0;
+	*p++ = 0.0;
+	*p++ = y;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 1.0;
+	*p++ = z;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 1.0;
+}
+
+void FillProjectionMatrix(double vx, double vy, double cx, double cy, double d, double(*matrix)[4])
+{
+	double *p = matrix;
+	*p++ = d * cx / vx;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = d * cy / vy;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 0.0;
+	*p++ = 1.0;
+	*p++ = 0.0;
+}
+
+void MatrixMultiplyM4(double(*mat1)[4], double(*mat2)[4]), double(*result)[4])
+{
+	int i, j, k;
+	double acc;
+
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			acc = 0.0;
+			for (k = 0; k < 4; k++)
+			{
+				acc += mat1[i][k] * mat2[k][j];
+			}
+			result[i][j] = acc;
+		}
+	}
+}
+
+void MatrixMultiplyV4(double(*matrix)[4], double *vector, double *result)
 {
 	int i, j;
 	double acc;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 4; i++)
 	{
 		acc = 0.0;
-		for (j = 0; j < 3; j++)
+		for (j = 0; j < 4; j++)
 		{
 			acc += vector[j] * matrix[i][j];
 		}
 		result[i] = acc;
 	}
+}
+
+void FillTransformMatrix(double x, double y, double z, double a, double b, double c, double s, double(*result)[4])
+{
+	double translation[4][4];
+	double rotation[4][4];
+	double scale[4][4];
+	double tmp[4][4];
+
+	FillTranslationMatrix(x, y, z, translation);
+	FillRotationMatrix(a, b, c, rotation);
+	FillScaleMatrix(s, s, s, scale);
+	MatrixMultiplyM4(rotation, scale, tmp);
+	MatrixMultiplyM4(translation, tmp, result);
 }
--- src/math/matrix.h
+++ src/math/matrix.h
@@ -1,4 +1,8 @@
 #pragma once
 
-void FillRotationMatrix(double, double, double, double(*)[3]);
-void MatrixMultiply3x3(double(*)[3], double *, double *);
+void FillRotationMatrix(double, double, double, double(*)[4]);
+void FillTranslationMatrix(double, double, double, double(*)[4]);
+void FillScaleMatrix(double, double, double, double(*)[4]);
+void FillTransformMatrix(double, double, double, double, double, double, double, double(*)[4]);
+void MatrixMultiplyM4(double(*)[4], double(*)[4], double(*)[4]);
+void MatrixMultiplyV4(double(*)[4], double*, double*);
