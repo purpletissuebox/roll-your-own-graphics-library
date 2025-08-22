5c0cbe48e51c31243d4d39f9c5f6f0bebfa4af42
Matthew Dirksen <dirksenmatthew1@gmail.com>
2025-08-21 22:58:52 -0400

    use new model definition, add w coordinate

--- src/models/pyramid.c
+++ src/models/pyramid.c
@@ -3,11 +3,11 @@
 #include "gfx/gfxbuffer.h"
 
 struct vertex PYRAMID_VERTICES[] = {
-	{1, 0, 1},
-	{1, 0, -1},
-	{-1, 0, -1},
-	{-1, 0, 1},
-	{0, 2, 0},
+	{{1, 0, 1, 1}},
+	{{1, 0, -1, 1}},
+	{{-1, 0, -1, 1}},
+	{{-1, 0, 1, 1}},
+	{{0, 2, 0, 1}},
 };
 
 struct face PYRAMID_FACES[] = {
@@ -15,13 +15,12 @@ struct face PYRAMID_FACES[] = {
 	{{1, 2, 4}, COLOR(0,0,5)},
 	{{2, 3, 4}, COLOR(5,0,0)},
 	{{3, 0, 4}, COLOR(5,5,0)},
-	{{0, 1, 2}, COLOR(0,5,5)},
-	{{2, 3, 0}, COLOR(0,5,5)},
+	{{0, 2, 1}, COLOR(0,5,5)},
+	{{0, 3, 2}, COLOR(0,5,5)},
 };
 
 struct model3d PYRAMID_MODEL = {
-	sizeof(PYRAMID_FACES)/sizeof(*PYRAMID_FACES),
-	sizeof(PYRAMID_VERTICES)/sizeof(*PYRAMID_VERTICES),
 	PYRAMID_FACES,
 	PYRAMID_VERTICES,
+	sizeof(PYRAMID_FACES)/sizeof(*PYRAMID_FACES),
 };
